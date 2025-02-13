bab1691f3b12596cb22180316b5fa0d4168694ba
Valiant <antero.lindell@gmail.com>
2025-02-13 03:31:35 +0200

    fixes to shell script

--- uus_idea.sh
+++ uus_idea.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ASM_SOURCE="RASPUTIN.ASM"
-OUTPUT_EXECUTABLE="RASPUTIN.bin"
+OUTPUT_EXECUTABLE="RASPUTIN"
 
 echo "Assembling $ASM_SOURCE..."
-nasm -f elf32 "$ASM_SOURCE" -o "${ASM_SOURCE%.asm}.o"
+nasm -f elf32 "$ASM_SOURCE" -o "${ASM_SOURCE%.ASM}.o"
 if [ $? -ne 0 ]; then
     echo "Assembly failed. Please check code."
     exit 1
 fi
 
-echo "Linking ${ASM_SOURCE%.asm}.o..."
-ld -m elf_i386 "${ASM_SOURCE%.asm}.o" -o "$OUTPUT_EXECUTABLE"
+echo "Linking ${ASM_SOURCE%.ASM}.o..."
+ld -m elf_i386 "${ASM_SOURCE%.ASM}.o" -o "$OUTPUT_EXECUTABLE"
 if [ $? -ne 0 ]; then
     echo "Linking failed. Please check your object file."
     exit 1
@@ -21,4 +21,6 @@ echo "Running $OUTPUT_EXECUTABLE..."
 ./"$OUTPUT_EXECUTABLE"
 
 echo "Cleaning up..."
-rm "${ASM_SOURCE%.asm}.o" "$OUTPUT_EXECUTABLE"
+rm "${ASM_SOURCE%.ASM}.o" "$OUTPUT_EXECUTABLE"
+
+./RASPUTIN
